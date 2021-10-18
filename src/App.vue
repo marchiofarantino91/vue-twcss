6ea38956b7bd27b85ebee73de521cb744e5520f8
marchiofarantino91 <marchiofarantino91@gmail.com>
2021-10-18 11:18:19 +0700

    2 files modified, 1 added, 1 deleted

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,27 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="https://vuejs.org/images/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<Header @changeTitle="ubahText" :text="text"/>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
+import Header from "./components/Header.vue"
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  components:{
+    Header
+  },
+  data(){
+    return{
+      text:"Selamat Datang dari Data!"
+    }
+  },
+methods:{
+  ubahText(newText){
+    this.text = newText;
   }
+}
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,28 @@
+<template>
+  <h2>Dinmana Mana saja</h2>
+  <h3>{{text}}</h3>
+  <button @click="changeText">Ganti Text AJA</button>
+</template>
+
+
+<script>
+export default {
+  name:"Header",
+ props:["text"],
+ data(){
+   return{
+     newText : "Ini Text Bagian Baru"
+   }
+ },
+ methods:{
+   changeText(){
+     this.$emit("changeTitle",this.newText);
+   }
+ },
+ emits:["changeTitle"]
+}
+</script>
+
+<style>
+
+</style>
